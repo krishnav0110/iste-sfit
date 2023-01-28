c910e42bc5000d28df3c39e90992721a2c2c2852
krishna vishwakarma <krishnav0110@gmail.com>
2023-01-28 18:45:20 +0530

    updated img, about fix

--- js/about.js
+++ js/about.js
@@ -1,13 +1,11 @@
 const img = document.querySelector('#about .desc-img img');
 
-let index = 0;
-let imgSrc = ["img/ISTE/19-20/Com2019.jpg", "img/sfitback.jpg"];
-
 const loopImg = () => {
-    setTimeout(() => {
-        index = index == 0 ? 1 : 0;
+    let index = 0;
+    let imgSrc = ["img/ISTE/22_23/team.jpg", "img/ISTE/19-20/Com2019.jpg", "img/sfitback.jpg"];
+    setInterval(() => {
+        index = ++index % imgSrc.length;
         img.src = imgSrc[index];
-        loopImg();
     }, 5000);
 };
 
